eba0f65bee8abfcb068544260457eb1354fa634e
hkwln <p.aul@mail.de>
2024-12-25 17:21:49 +0100

    changed so that the data is from tf.keras and not tfds

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import tensorflow as tf
 import numpy as np
-import tensorflow_datasets as tfds
 
 # Load the dataset
-(mnist_train, mnist_test), mnist_dataset = tfds.load(name = "mnist", split = ['train','test'], shuffle_files = True, as_supervised=True, with_info=True)
-# Split the dataset into training and testing and the belonging labels
+mnist = tf.keras.datasets.mnist
 
-def prepare_data(dataset):
-  images, labels = [], []
-  for image, label in dataset.as_numpy_iterator():
-    image = np.array(image) / 255.0 # Normalize the image
-    image = np.array(image)  # Ensure image is a numpy array
-    image = image.reshape(-1)  # Reshape to (28, 28, 1)
-    images.append(image)
-    labels.append(label)
-  return np.array(images, dtype=np.float32), np.array(labels, dtype=np.float32)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Prepare the training and testing data
-x_train, y_train = prepare_data(mnist_train)
-x_test, y_test = prepare_data(mnist_test)
+train_ds = tf.data.Dataset.from_tensor_slices(
+    (x_train, y_train)).shuffle(10000).batch(32)
 
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+# Add a channels dimension
+x_train = x_train[..., tf.newaxis].astype("float32")
+x_test = x_test[..., tf.newaxis].astype("float32")
 # build the model
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
@@ -28,17 +22,21 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 predictions = model(x_train[:1]).numpy()
-predictions
 tf.nn.softmax(predictions).numpy()
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
 loss_fn(y_train[:1], predictions).numpy()
 
-model.compile(optimizer='adam',
-                loss=loss_fn,
+model.compile(optimizer= tf.keras.optimizers.Adam(learning_rate=0.001),
+                loss= loss_fn,
                 metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=5)
 
 model.evaluate(x_test, y_test, verbose=2)
+
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+probability_model(x_test[:5])
